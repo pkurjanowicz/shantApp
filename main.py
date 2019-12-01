e5a00db8830283cf4e8e7f81e45568251050e4ca
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2019-12-01 12:13:53 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, render_template
+from twilio.twiml.messaging_response import MessagingResponse
+import datetime
+
+app = Flask(__name__)
+
+shants_num = '+18186068167'
+last_poop_date = ''
+
+def verify_shant(number):
+    if number == '+12032312081':
+        return True
+    return False
+
+def update_status():
+    time_object = datetime.datetime.today()
+    date_today = '{}-{}-{}'.format(time_object.month,time_object.day,time_object.year)
+    global last_poop_date
+    if date_today != last_poop_date:
+        return 'No'
+    return 'Yes!'
+
+@app.route('/', methods=['GET'])
+def render_app():
+    has_shant_pooped = update_status()
+    if has_shant_pooped == 'Yes!':
+        return render_template('index.html', message='Yes', last_poop_date=last_poop_date)
+    return render_template('index.html', message="No", last_poop_date=last_poop_date)
+
+@app.route("/sms", methods=['GET'])
+def sms_ahoy_reply():
+    number = request.args.get('From')
+    body = request.args.get('Body')
+    if verify_shant(number):
+        resp = MessagingResponse()
+        resp.message("Thanks for updating your pooping status")
+        time_object = datetime.datetime.today()
+        date_today = '{}-{}-{}'.format(time_object.month,time_object.day,time_object.year)
+        global last_poop_date
+        last_poop_date = date_today
+        return str(resp)
+    return 'Falure'
+
+    """
+    RESPONSE DATA EXAMPLE(QUERY STRING)
+    ?ToCountry=US
+    &ToState=AL
+    &SmsMessageSid=SMc5c48883a3902d997b64e39dca6bd5c9
+    &NumMedia=0
+    &ToCity=TUSCALOOSA
+    &FromZip=06484
+    &SmsSid=SMc5c48883a3902d997b64e39dca6bd5c9
+    &FromState=CT
+    &SmsStatus=received
+    &FromCity=DERBY
+    &Body=hey+there
+    &FromCountry=US
+    &To=%2B12052933232
+    &ToZip=35405
+    &NumSegments=1
+    &MessageSid=SMc5c48883a3902d997b64e39dca6bd5c9
+    &AccountSid=ACd11feb6ef2a175264fd6e40fc62ad19d
+    &From=%2B12032312081
+    &ApiVersion=2010-04-01
+    """
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
